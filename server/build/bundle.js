9eafa424eba870a49f77cb2e477c5e4f4160ae2e
Alan Leverenz <awleverenz@aol.com>
2021-01-04 16:06:16 -0500

    hydrated the client.js file to refresh the server's content

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -8247,7 +8247,7 @@ app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
     var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-    var html = '\n        <html>\n            <head></head>\n            <body>\n                <div>' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n    ';
+    var html = '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n    ';
 
     res.send(html);
 });
--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,4 +1,14 @@
-// Startup point for the client side application
+// Startup point for the client side application - bootup location
 
-console.log('Hi there!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+// rendering into the same div as the index.js file does
+// hydrate is used to refresh what the server sent to the browser
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+
+
+
+
 

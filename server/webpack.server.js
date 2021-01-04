d9824891399c70c9e4b02dc96af517cf5ef8ada9
Alan Leverenz <awleverenz@aol.com>
2021-01-04 16:43:10 -0500

    used webpackNodeExternals to ignore libaries in node_modules

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // inform webpack that we're building a bundle for nodeJS, instead for the browser
@@ -13,7 +14,10 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    // does not insert libraries into server bundle file if library already exist in node modules folder
+    externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);

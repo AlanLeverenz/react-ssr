d379d5d1a82b9390d5817c5adc2ec31f53bcc07e
Alan Leverenz <awleverenz@aol.com>
2021-01-07 00:07:21 -0500

    updated renderer.js to add location

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -131,19 +131,19 @@ var _server = __webpack_require__(4);
 
 var _reactRouterDom = __webpack_require__(6);
 
-var _Routers = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"../client/Routers\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+var _Routes = __webpack_require__(7);
 
-var _Routers2 = _interopRequireDefault(_Routers);
+var _Routes2 = _interopRequireDefault(_Routes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Home component being rendered by Routes compoennt
 // import Home from '../client/components/Home';
-exports.default = function () {
+exports.default = function (req) {
     var content = (0, _server.renderToString)(_react2.default.createElement(
         _reactRouterDom.StaticRouter,
-        { context: {} },
-        _react2.default.createElement(_Routers2.default, null)
+        { location: req.path, context: {} },
+        _react2.default.createElement(_Routes2.default, null)
     ));
 
     return '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n        </html>    \n    ';
@@ -156,11 +156,80 @@ exports.default = function () {
 module.exports = require("react-dom/server");
 
 /***/ }),
-/* 5 */,
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Home = function Home() {
+    return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+            'div',
+            null,
+            'I\'m the NEW home component'
+        ),
+        _react2.default.createElement(
+            'button',
+            { onClick: function onClick() {
+                    return console.log('Hi there!');
+                } },
+            'Press me!'
+        )
+    );
+};
+
+exports.default = Home;
+
+/***/ }),
 /* 6 */
 /***/ (function(module, exports) {
 
 module.exports = require("react-router-dom");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouterDom = __webpack_require__(6);
+
+var _Home = __webpack_require__(5);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// <div> below only rendered on the client
+exports.default = function () {
+    return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: '/', component: _Home2.default })
+    );
+};
 
 /***/ })
 /******/ ]);
--- server/src/helpers.js/renderer.js
+++ server/src/helpers.js/renderer.js
@@ -3,11 +3,11 @@ import { renderToString } from 'react-dom/server';
 // Home component being rendered by Routes compoennt
 // import Home from '../client/components/Home';
 import { StaticRouter } from 'react-router-dom';
-import Routes from '../client/Routers';
+import Routes from '../client/Routes';
 
-export default () => {
+export default (req) => {
     const content = renderToString(
-        <StaticRouter context={{}}>
+        <StaticRouter location={req.path} context={{}}>
             <Routes />
         </StaticRouter>
     );

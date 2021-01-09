1fb48ef5fc06f938f28b778865247b1b48f67b08
Alan Leverenz <awleverenz@aol.com>
2021-01-08 21:12:18 -0500

    client side redux store setup

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -4,18 +4,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Home from './components/Home';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'; // for asynchronous data actions
+import { Provider } from 'react-redux'; // gather state from store
 import Routes from './Routes';
 
 // rendering into the same div as the index.js file does
 // hydrate is used to refresh what the server sent to the browser
 // don't need to import Home component because it is used in Routes
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// Provider updates the root app with changes to store
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root'));
-
-
-
-
-

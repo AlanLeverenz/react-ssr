734c7c8d0feb1afe06156a3288ecb81a231d13f4
Alan Leverenz <awleverenz@aol.com>
2021-01-18 21:57:39 -0500

    used serialize-javascript as xss attack prevention

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -288,6 +288,10 @@ var _reactRedux = __webpack_require__(3);
 
 var _reactRouterConfig = __webpack_require__(1);
 
+var _serializeJavascript = __webpack_require__(21);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _Routes = __webpack_require__(2);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -296,8 +300,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // Provider gets state updates from store
 
-// Home component being rendered by Routes compoennt
-// import Home from '../client/components/Home';
+// to protect against XSS attacks
 exports.default = function (req, store) {
     var content = (0, _server.renderToString)(_react2.default.createElement(
         _reactRedux.Provider,
@@ -313,8 +316,10 @@ exports.default = function (req, store) {
         )
     ));
 
-    return '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script>\n                    window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '\n                </script>\n                <script src="bundle.js"></script>\n            </body>\n        </html>    \n    ';
+    return '\n        <html>\n            <head></head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script>\n                    window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n                </script>\n                <script src="bundle.js"></script>\n            </body>\n        </html>    \n    ';
 };
+// Home component being rendered by Routes compoennt
+// import Home from '../client/components/Home';
 
 /***/ }),
 /* 13 */
@@ -542,6 +547,12 @@ exports.default = {
     loadData: loadData,
     component: (0, _reactRedux.connect)(mapStateToProps, { fetchUsers: _actions.fetchUsers })(UsersList)
 };
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);

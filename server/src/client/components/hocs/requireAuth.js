1616084d1cd099e63e46c2982df4b2b409972d01
Alan Leverenz <awleverenz@aol.com>
2021-01-29 13:27:01 -0500

    implemented require auth

---
+++ server/src/client/components/hocs/requireAuth.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// will send the user to another place in the app
+import { Redirect } from 'react-router-dom';
+
+export default (ChildComponent) => {
+    class RequireAuth extends Component {
+        render() {
+            switch (this.props.auth ) {
+                case false:
+                    return <Redirect to="/" />;
+
+                // still figuring out what to do
+                case null:
+                    return <div>Loading...</div>
+                
+                // forward props through to the child component
+                default: 
+                    return <ChildComponent {...this.props} />;
+
+            }
+        }
+    }
+
+    // checks to see if user is authenticated
+    function mapStateToProps({ auth }) {
+        return { auth };
+    }
+
+    // connect helper wraps the component just created
+    // export final wrapped version of the component
+    return connect(mapStateToProps)(RequireAuth);
+
+}
